5681fd5e2e7de64e8808de1aadc08fded1c79854
flashlin <flash.lin@gmail.com>
2021-02-24 20:04:52 +0800

    add Select_field_add_1_from_table

--- T1.ParserKitTests/SqlParserSelectTest.cs
+++ T1.ParserKitTests/SqlParserSelectTest.cs
@@ -150,62 +150,38 @@ namespace T1.ParserKitTests
 			});
 		}
 
-		//[Fact]
-		//public void Select_field_add_1_from_table()
-		//{
-		//	GiveText("select id+1 from customer");
-		//	WhenParse();
-		//	ThenResultShouldBe(new SelectExpression()
-		//	{
-		//		File = "",
-		//		Length = 25,
-		//		Position = 0,
-		//		Content = _code,
-		//		Fields = new FieldsExpression()
-		//		{
-		//			File = "",
-		//			Length = 4,
-		//			Position = 7,
-		//			Content = _code,
-		//			Items = new List<SqlExpression>()
-		//			{
-		//				new ArithmeticOperatorExpression
-		//				{
-		//					Left = new FieldExpression
-		//					{
-		//						Name = "id",
-		//						File = "",
-		//						Length = 2,
-		//						Position = 7,
-		//						Content = _code
-		//					},
-		//					Oper = "+",
-		//					Right = new NumberExpression
-		//					{
-		//						Value = 1,
-		//						ValueTypeFullname = typeof(int).FullName,
-		//						File = "",
-		//						Length = 1,
-		//						Position = 10,
-		//						Content = _code
-		//					},
-		//					File = "",
-		//					Length = 4,
-		//					Position = 7,
-		//					Content = _code
-		//				}
-		//			}
-		//		},
-		//		From = new TableExpression()
-		//		{
-		//			File = "",
-		//			Length = 8,
-		//			Position = 17,
-		//			Content = _code,
-		//			Name = "customer"
-		//		}
-		//	});
-		//}
+		[Fact]
+		public void Select_field_add_1_from_table()
+		{
+			GiveText("select id+1 from customer");
+			WhenParse(SqlParser.SelectExpr);
+			ThenResultShouldBe(new SelectExpression()
+			{
+				Fields = new FieldsExpression()
+				{
+					Items = new List<SqlExpression>()
+					{
+						new ArithmeticOperatorExpression
+						{
+							Left = new FieldExpression
+							{
+								Name = "id",
+							},
+							Oper = "+",
+							Right = new NumberExpression
+							{
+								Value = 1,
+								ValueTypeFullname = typeof(int).FullName,
+							},
+						}
+					}
+				},
+				From = new TableExpression()
+				{
+					Name = "customer"
+				}
+			});
+		}
 
 
 
