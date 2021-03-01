50f5930610c744aa1e6c6a99795564dba6b6b3bf
flashlin <flash.lin@gmail.com>
2021-03-01 16:19:21 +0800

    WhereTest

---
+++ T1.ParserKitTests/WhereTest.cs
@@ -0,0 +1,32 @@
+﻿using T1.ParserKit.SqlDom;
+using T1.ParserKit.SqlDom.Expressions;
+using Xunit;
+
+namespace T1.ParserKitTests
+{
+	public class WhereTest : ParseTestBase
+	{
+		[Fact]
+		public void Where_name_eq_1()
+		{
+			GiveText("where name = 1");
+			WhenParse(SqlParser.WhereExpr);
+			ThenResultShouldBe(new WhereExpression()
+			{
+				Filter = new FilterExpression()
+				{
+					Left = new FieldExpression()
+					{
+						Name = "name"
+					},
+					Oper = "=",
+					Right = new NumberExpression()
+					{
+						ValueTypeFullname = typeof(int).FullName,
+						Value = 1
+					}
+				}
+			});
+		}
+	}
+}
