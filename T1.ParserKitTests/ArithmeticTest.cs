846ae319a0b5e22a9980792c30ba32fd09575c5e
flashlin <flash.lin@gmail.com>
2021-03-01 16:16:56 +0800

    Arithmetic_Group_LParen_1_add_2_RParen_mul_3

--- T1.ParserKitTests/ArithmeticTest.cs
+++ T1.ParserKitTests/ArithmeticTest.cs
@@ -96,5 +96,34 @@ namespace T1.ParserKitTests
 			});
 		}
 
+		[Fact]
+		public void Arithmetic_Group_LParen_1_add_2_RParen_mul_3()
+		{
+			GiveText("(1 + 2) * 3");
+			WhenParse(SqlParser.ArithmeticOperatorAtomExpr);
+			ThenResultShouldBe(new ArithmeticOperatorExpression()
+			{
+				Left = new ArithmeticOperatorExpression
+				{
+					Left = new NumberExpression
+					{
+						Value = 1,
+						ValueTypeFullname = typeof(int).FullName,
+					},
+					Oper = "+",
+					Right = new NumberExpression
+					{
+						Value = 2,
+						ValueTypeFullname = typeof(int).FullName,
+					},
+				},
+				Oper = "*",
+				Right = new NumberExpression
+				{
+					Value = 3,
+					ValueTypeFullname = typeof(int).FullName,
+				}
+			});
+		}
 	}
 }
